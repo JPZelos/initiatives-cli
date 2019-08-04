5bf0481582e2fb13531bb55aabd86f5f27516600
JPZelos <i.p.zelos@gmail.com>
2019-08-04 16:48:28 +0300

    Refactors main to accept multiple characters and initiative modifiers

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const dices = require("./dices");
+const initiativeRoller = require("./initiativeRoller");
+const utils = require("./utils");
 
 function main() {
-  const d20Result = dices.d20();
-  console.log("d20 rolled: " + d20Result);
+  const args = process.argv.slice(2);
+  const names = args.filter((value, index) => utils.isEven(index));
+  const mods = args.filter((value, index) => utils.isOdd(index));
+  let characters = utils.zip(names, mods);
+  characters = characters.map(value => {
+    const mod = parseInt(value[1], 10);
+    const initiative = initiativeRoller(dices.d20, mod);
+    return [value[0], initiative];
+  });
+  characters.sort((a, b) => b[1] - a[1]);
+  console.log(characters);
 }
 
 main();
